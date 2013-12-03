3972b5226253e9fe723e3a6942499c527614aaec
f00barin <pranava.madhyastha@gmail.com>
2013-12-03 16:30:15 +0100

    fixed bugs

--- rcmaxent.py
+++ rcmaxent.py
@@ -4,7 +4,7 @@ import scipy.io as sio
 import numpy as np
 import combo_me as co
 #import maxent_new as maxent
-import maxent as maxent
+import maxentnew as maxent
 import sys
 
 samples = int(sys.argv[1])
@@ -44,23 +44,16 @@ def increasing(L):
 def minlog(L):
     val, idx = min((val, idx) for (idx, val) in enumerate(L))
     return val
-
-tau_vals_a = [100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001, 0.000000001, 0.0000000001]
-tau_vals_b = [0.1, 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000, 1000000000000]
-tau_vals_c = [100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000, 1000000000000, 1000000000000, 10000000000000,100000000000000 ]
-eta = [1000, 500, 100, 50, 10, 1, 0.5,  0.1, 0.05, 0.01, 0.001, 0.0001]
-
-cls = 1
-cb = 1
-e = 12
-tau = 1
+tau_vals = [1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.000000001, 0.0000000001, 0.00000000001]
+eta = [100]
 if inp == 'None':
     fmins = {}
+    tau = 1
     for e in eta:
         main_string = str(e)+','+str(tau)
         try:
             print 'calling function type ', inp, ' with tau = ', tau, ' and eta = ', e
-            cl = maxent.Maxent.train(train_toks=traintokens, algorithm='gd', max_iter=100, eta=e, devset=devtokens, tau=tau)
+            cl = maxent.Maxent.train(train_toks=traintokens, algorithm='gd', max_iter=40, eta=e, devset=devtokens, tau=tau)
             np.savetxt('lintracc25'+inp+str(samples)+'tau'+str(tau)+'eta'+str(e)+'.txt', cl[1], fmt='%f')
             np.savetxt('linlog25'+inp+str(samples)+'tau'+str(tau)+'eta'+str(e)+'.txt', cl[2], fmt='%f')
             np.savetxt('lindevacc25'+inp+str(samples)+'tau'+str(tau)+'eta'+str(e)+'.txt', cl[3], fmt='%f')
@@ -80,18 +73,18 @@ if inp == 'None':
             np.savetxt('tracc'+inp+str(samples)+'tau'+val[1]+'eta'+val[0]+'.txt', cl[1], fmt='%f')
             np.savetxt('wtln'+inp+str(samples)+'tau'+val[1]+'eta'+val[0]+'.txt', cl[0].weights_n(), fmt='%f')
             np.savetxt('wtlv'+inp+str(samples)+'tau'+val[1]+'eta'+val[0]+'.txt', cl[0].weights_v(), fmt='%f')
-            break
+
         except:
             pass
 
 elif inp == 'l2':
     fmins = {}
-    for tau in tau_vals_a:
+    for tau in tau_vals:
         for e in eta:
             main_string = str(e)+','+str(tau)
             try:
                 print 'calling function type ', inp, ' with tau = ', tau, ' and eta = ', e
-                cl = maxent.Maxent.train(train_toks=traintokens, algorithm='gd', max_iter=100, eta=e, devset=devtokens, tau=tau, norm='l2')
+                cl = maxent.Maxent.train(train_toks=traintokens, algorithm='gd', max_iter=40, eta=e, devset=devtokens, tau=tau, norm='l2')
                 np.savetxt('lintracc25'+inp+str(samples)+'tau'+str(tau)+'eta'+str(e)+'.txt', cl[1], fmt='%f')
                 np.savetxt('linlog25'+inp+str(samples)+'tau'+str(tau)+'eta'+str(e)+'.txt', cl[2], fmt='%f')
                 np.savetxt('lindevacc25'+inp+str(samples)+'tau'+str(tau)+'eta'+str(e)+'.txt', cl[3], fmt='%f')
@@ -111,24 +104,24 @@ elif inp == 'l2':
             np.savetxt('tracc'+inp+str(samples)+'tau'+val[1]+'eta'+val[0]+'.txt', cl[1], fmt='%f')
             np.savetxt('wtln'+inp+str(samples)+'tau'+val[1]+'eta'+val[0]+'.txt', cl[0].weights_n(), fmt='%f')
             np.savetxt('wtlv'+inp+str(samples)+'tau'+val[1]+'eta'+val[0]+'.txt', cl[0].weights_v(), fmt='%f')
-            break
+
         except:
             pass
 
 elif inp == 'l1':
+    lc = 0.05
     fmins = {}
-    for lc in tau_vals_c:
-        for tau in tau_vals_b:
-            main_string = str(lc)+','+str(tau)
-            try:
-                print 'calling function type ', inp, ' with tau = ', tau, ' and LC = ', lc
-                cl = maxent.Maxent.train(train_toks=traintokens, algorithm='gd', max_iter=100, devset=devtokens, tau=tau, norm='l1', LC=lc)
-                np.savetxt('lintracc25'+inp+str(samples)+'tau'+str(tau)+'lc'+str(lc)+'.txt', cl[1], fmt='%f')
-                np.savetxt('linlog25'+inp+str(samples)+'tau'+str(tau)+'lc'+str(lc)+'.txt', cl[2], fmt='%f')
-                np.savetxt('lindevacc25'+inp+str(samples)+'tau'+str(tau)+'lc'+str(lc)+'.txt', cl[3], fmt='%f')
-                fmins[main_string] = minlog(cl[2])
-            except:
-                print 'problem with LC = ', lc, ' tau = ', tau
+    for tau in tau_vals:
+        main_string = str(lc)+','+str(tau)
+        try:
+            print 'calling function type ', inp, ' with tau = ', tau, ' and LC = ', lc
+            cl = maxent.Maxent.train(train_toks=traintokens, algorithm='gd', max_iter=40, devset=devtokens, tau=tau, norm='l1', LC=lc)
+            np.savetxt('lintracc25'+inp+str(samples)+'tau'+str(tau)+'lc'+str(lc)+'.txt', cl[1], fmt='%f')
+            np.savetxt('linlog25'+inp+str(samples)+'tau'+str(tau)+'lc'+str(lc)+'.txt', cl[2], fmt='%f')
+            np.savetxt('lindevacc25'+inp+str(samples)+'tau'+str(tau)+'lc'+str(lc)+'.txt', cl[3], fmt='%f')
+            fmins[main_string] = minlog(cl[2])
+        except:
+            print 'problem with LC = ', lc, ' tau = ', tau
 
     mstr = sorted(fmins, key=fmins.get)
     final = mstr
@@ -142,7 +135,38 @@ elif inp == 'l1':
             np.savetxt('tracc'+inp+str(samples)+'tau'+val[1]+'lc'+val[0]+'.txt', cl[1], fmt='%f')
             np.savetxt('wtln'+inp+str(samples)+'tau'+val[1]+'lc'+val[0]+'.txt', cl[0].weights_n(), fmt='%f')
             np.savetxt('wtlv'+inp+str(samples)+'tau'+val[1]+'lc'+val[0]+'.txt', cl[0].weights_v(), fmt='%f')
-            break
+
+        except:
+            pass
+
+elif inp == 'l2proximal':
+    lc = 0.05
+    fmins = {}
+    for tau in tau_vals:
+        main_string = str(lc)+','+str(tau)
+        try:
+            print 'calling function type ', inp, ' with tau = ', tau, ' and LC = ', lc
+            cl = maxent.Maxent.train(train_toks=traintokens, algorithm='gd', max_iter=40, devset=devtokens, tau=tau, norm='l2proximal', LC=lc)
+            np.savetxt('lintracc25'+inp+str(samples)+'tau'+str(tau)+'lc'+str(lc)+'.txt', cl[1], fmt='%f')
+            np.savetxt('linlog25'+inp+str(samples)+'tau'+str(tau)+'lc'+str(lc)+'.txt', cl[2], fmt='%f')
+            np.savetxt('lindevacc25'+inp+str(samples)+'tau'+str(tau)+'lc'+str(lc)+'.txt', cl[3], fmt='%f')
+            fmins[main_string] = minlog(cl[2])
+        except:
+            print 'problem with LC = ', lc, ' tau = ', tau
+
+    mstr = sorted(fmins, key=fmins.get)
+    final = mstr
+    print final
+    for i in range(len(final)):
+        try:
+            val = (final[i]).split(',')
+            cl = maxent.Maxent.train(train_toks=traintokens, algorithm='gd', max_iter=100, LC=float(val[0]), devset=devtokens, tau=float(val[1]), norm='l2proximal')
+            np.savetxt('devacc'+inp+str(samples)+'tau'+val[1]+'lc'+val[0]+'.txt', cl[3], fmt='%f')
+            np.savetxt('neglogl'+inp+str(samples)+'tau'+val[1]+'lc'+val[0]+'.txt', cl[2], fmt='%f')
+            np.savetxt('tracc'+inp+str(samples)+'tau'+val[1]+'lc'+val[0]+'.txt', cl[1], fmt='%f')
+            np.savetxt('wtln'+inp+str(samples)+'tau'+val[1]+'lc'+val[0]+'.txt', cl[0].weights_n(), fmt='%f')
+            np.savetxt('wtlv'+inp+str(samples)+'tau'+val[1]+'lc'+val[0]+'.txt', cl[0].weights_v(), fmt='%f')
+
         except:
             pass
 
