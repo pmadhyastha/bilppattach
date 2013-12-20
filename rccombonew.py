0b0064c9c553e34bdcd59e7abe2d44411401c29e
f00barin <pranava.madhyastha@gmail.com>
2013-12-20 12:27:10 +0100

    addding new rccombo.py

---
+++ rccombonew.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+
+import scipy.io as sio
+import numpy as np
+import combo_me as co
+#import maxent_new as maxent
+import bilinear_me as bilme
+import sys
+
+samples = int(sys.argv[1])
+inp = str(sys.argv[2])
+eta = float(sys.argv[3])
+tau_l = float(sys.argv[4])
+tau_b = float(sys.argv[5])
+numbers = int(sys.argv[6])
+ppt = str(sys.argv[7])
+print 'pptype = ', ppt
+#if float(sys.argv[5]):
+#    eta2 = float(sys.argv[5])
+#    eta = np.random.uniform(float(sys.argv[3]), float(sys.argv[5]), 10)
+#else:
+#    eta = eta1
+
+traindata = [(d.strip().split()[1:5], d.strip().split()[5]) for d in open('clean/cleantrain.txt')]
+devdata = [(d.strip().split()[1:5], d.strip().split()[5]) for d in open('clean/cleandev.txt')]
+testdata = [(d.strip().split()[1:5], d.strip().split()[5]) for d in open('clean/cleantest.txt')]
+traindata = traindata[:samples]
+phih = sio.mmread('clean/trh1k.mtx')
+phim = sio.mmread('clean/trm1k.mtx')
+phidh = sio.mmread('clean/devh1k.mtx')
+phidm = sio.mmread('clean/devm1k.mtx')
+maph = np.loadtxt('clean/forhead.txt', dtype=str)
+mapm = np.loadtxt('clean/formod.txt', dtype=str)
+mapdh = np.loadtxt('clean/devheads.txt', dtype=str)
+mapdm = np.loadtxt('clean/devmods.txt', dtype=str)
+
+
+encoding = co.ComboMaxentFeatEncoding.train(traindata, phih, phim, maph, mapm, pptype=ppt)
+traintoks = encoding.train_toks()
+traintokens = [(co.word_features(t),l) for t,l in encoding.train_toks()]
+print 'type = ', inp, 'total samples = ', samples
+print "total training examples for the pptype - None ", len(traintoks)
+
+devencode = co.ComboMaxentFeatEncoding.train(devdata, phidh, phidm, mapdh, mapdm, pptype=ppt)
+devtoks = devencode.train_toks()
+devtokens = [(co.word_features(t),l) for t,l in devencode.train_toks()]
+print "total development examples for the pptype - None ", len(devtoks)
+
+def decreasing(L):
+    return all(x>=y for x, y in zip(L, L[1:]))
+
+def increasing(L):
+    return all(x<=y for x, y in zip(L, L[1:]))
+
+def minlog(L):
+    val, idx = min((val, idx) for (idx, val) in enumerate(L))
+    return val
+
+if inp == 'None':
+    print 'calling function type ', inp, ' With tau_l and tau_b = ', tau_l, tau_b, ' and eta = ', eta
+
+    cl = co.ComboMaxent.combo_train(traintoks, encoding, max_iter=numbers, eta=eta, devset=devtoks, devencode=devencode, tau_l=tau_l, tau_b=tau_b)
+    np.savetxt('combo-models/comlog'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'eta'+str(eta)+'.txt', cl[2], fmt='%f')
+    np.savetxt('combo-models/comtracc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'eta'+str(eta)+'.txt', cl[1], fmt='%f')
+    np.savetxt('combo-models/comdevacc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'eta'+str(eta)+'.txt', cl[3], fmt='%f')
+    sio.mmwrite('combo-models/comwtbn'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'eta'+str(eta)+'.mtx', cl[0].weight_bn())
+    sio.mmwrite('combo-models/comwtbv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'eta'+str(eta)+'.mtx', cl[0].weight_bv())
+    np.savetxt('combo-models/comwtln'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'eta'+str(eta)+'.txt', cl[0].weight_ln(), fmt='%f')
+    np.savetxt('combo-models/comwtlv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'eta'+str(eta)+'.txt', cl[0].weight_lv(), fmt='%f')
+
+if inp == 'l2pl2p':
+    print 'calling function type ', inp, ' With tau_l and tau_b = ', tau_l, tau_b, ' and lc = ', eta
+
+    cl = co.ComboMaxent.combo_train(traintoks, encoding, max_iter=numbers, devset=devtoks, devencode=devencode, tau_l=tau_l, tau_b=tau_b, l_penalty='l2p', b_penalty='l2p', LC_l=eta, LC_b=eta)
+    np.savetxt('combo-models/comlog'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[2], fmt='%f')
+    np.savetxt('combo-models/comtracc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[1], fmt='%f')
+    np.savetxt('combo-models/comdevacc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[3], fmt='%f')
+    sio.mmwrite('combo-models/comwtbn'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bn())
+    sio.mmwrite('combo-models/comwtbv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bv())
+    np.savetxt('combo-models/comwtln'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_ln(), fmt='%f')
+    np.savetxt('combo-models/comwtlv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_lv(), fmt='%f')
+
+if inp == 'l2pl1':
+    print 'calling function type ', inp, ' With tau_l and tau_b = ', tau_l, tau_b, ' and lc = ', eta
+
+    cl = co.ComboMaxent.combo_train(traintoks, encoding, max_iter=numbers, devset=devtoks, devencode=devencode, tau_l=tau_l, tau_b=tau_b, l_penalty='l2p', b_penalty='l1', LC_l=eta, LC_b=eta)
+    np.savetxt('combo-models/comlog'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[2], fmt='%f')
+    np.savetxt('combo-models/comtracc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[1], fmt='%f')
+    np.savetxt('combo-models/comdevacc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[3], fmt='%f')
+    sio.mmwrite('combo-models/comwtbn'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bn())
+    sio.mmwrite('combo-models/comwtbv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bv())
+    np.savetxt('combo-models/comwtln'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_ln(), fmt='%f')
+    np.savetxt('combo-models/comwtlv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_lv(), fmt='%f')
+
+
+if inp == 'l2pnn':
+    print 'calling function type ', inp, ' With tau_l and tau_b = ', tau_l, tau_b, ' and lc = ', eta
+
+    cl = co.ComboMaxent.combo_train(traintoks, encoding, max_iter=numbers, devset=devtoks, devencode=devencode, tau_l=tau_l, tau_b=tau_b, l_penalty='l2p', b_penalty='nn', LC_l=eta, LC_b=eta)
+    np.savetxt('combo-models/comlog'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[2], fmt='%f')
+    np.savetxt('combo-models/comtracc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[1], fmt='%f')
+    np.savetxt('combo-models/comdevacc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[3], fmt='%f')
+    sio.mmwrite('combo-models/comwtbn'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bn())
+    sio.mmwrite('combo-models/comwtbv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bv())
+    np.savetxt('combo-models/comwtln'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_ln(), fmt='%f')
+    np.savetxt('combo-models/comwtlv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_lv(), fmt='%f')
+
+
+if inp == 'l1nn':
+    print 'calling function type ', inp, ' With tau_l and tau_b = ', tau_l, tau_b, ' and lc = ', eta
+
+    cl = co.ComboMaxent.combo_train(traintoks, encoding, max_iter=numbers, devset=devtoks, devencode=devencode, tau_l=tau_l, tau_b=tau_b, l_penalty='l1', b_penalty='nn', LC_l=eta, LC_b=eta)
+    np.savetxt('combo-models/comlog'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[2], fmt='%f')
+    np.savetxt('combo-models/comtracc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[1], fmt='%f')
+    np.savetxt('combo-models/comdevacc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[3], fmt='%f')
+    sio.mmwrite('combo-models/comwtbn'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bn())
+    sio.mmwrite('combo-models/comwtbv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bv())
+    np.savetxt('combo-models/comwtln'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_ln(), fmt='%f')
+    np.savetxt('combo-models/comwtlv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_lv(), fmt='%f')
+
+if inp == 'l1l1':
+    print 'calling function type ', inp, ' With tau_l and tau_b = ', tau_l, tau_b, ' and lc = ', eta
+
+    cl = co.ComboMaxent.combo_train(traintoks, encoding, max_iter=numbers, devset=devtoks, devencode=devencode, tau_l=tau_l, tau_b=tau_b, l_penalty='l1', b_penalty='l1', LC_l=eta, LC_b=eta)
+    np.savetxt('combo-models/comlog'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[2], fmt='%f')
+    np.savetxt('combo-models/comtracc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[1], fmt='%f')
+    np.savetxt('combo-models/comdevacc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[3], fmt='%f')
+    sio.mmwrite('combo-models/comwtbn'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bn())
+    sio.mmwrite('combo-models/comwtbv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bv())
+    np.savetxt('combo-models/comwtln'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_ln(), fmt='%f')
+    np.savetxt('combo-models/comwtlv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_lv(), fmt='%f')
+
+
+if inp == 'l1l2p':
+    print 'calling function type ', inp, ' With tau_l and tau_b = ', tau_l, tau_b, ' and lc = ', eta
+
+    cl = co.ComboMaxent.combo_train(traintoks, encoding, max_iter=numbers, devset=devtoks, devencode=devencode, tau_l=tau_l, tau_b=tau_b, l_penalty='l1', b_penalty='l2p', LC_l=eta, LC_b=eta)
+    np.savetxt('combo-models/comlog'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[2], fmt='%f')
+    np.savetxt('combo-models/comtracc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[1], fmt='%f')
+    np.savetxt('combo-models/comdevacc'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[3], fmt='%f')
+    sio.mmwrite('combo-models/comwtbn'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bn())
+    sio.mmwrite('combo-models/comwtbv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.mtx', cl[0].weight_bv())
+    np.savetxt('combo-models/comwtln'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_ln(), fmt='%f')
+    np.savetxt('combo-models/comwtlv'+inp+str(samples)+'cl'+str(tau_l)+'cb'+str(tau_b)+'lc'+str(eta)+'.txt', cl[0].weight_lv(), fmt='%f')
+
