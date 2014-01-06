f09babdf8d4b54c2fb9ac006956c32418b4e3ce8
f00barin <pranava.madhyastha@gmail.com>
2014-01-06 17:40:00 +0100

    fixed the ppt disappearing problem

--- rcbilinear.py
+++ rcbilinear.py
@@ -11,6 +11,11 @@ samples = int(sys.argv[1])
 inp = str(sys.argv[2])
 eta = float(sys.argv[3])
 tau = float(sys.argv[4])
+numbers = int(sys.argv[5])
+ppt = str(sys.argv[6])
+
+print 'pptype = ', ppt
+
 #if float(sys.argv[5]):
 #    eta2 = float(sys.argv[5])
 #    eta = np.random.uniform(float(sys.argv[3]), float(sys.argv[5]), 10)
@@ -31,12 +36,12 @@ mapdh = np.loadtxt('clean/devheads.txt', dtype=str)
 mapdm = np.loadtxt('clean/devmods.txt', dtype=str)
 
 
-encoding = bilme.BilinearMaxentFeatEncoding.train(traindata, phih, phim, maph, mapm, pptype='for')
+encoding = bilme.BilinearMaxentFeatEncoding.train(traindata, phih, phim, maph, mapm, pptype=ppt)
 traintoks = encoding.train_toks()
 traintokens = [(co.word_features(t),l) for t,l in encoding.train_toks()]
 print 'type = ', inp, 'total samples = ', samples
 print "total training examples for the pptype - None ", len(traintoks)
-devencode = bilme.BilinearMaxentFeatEncoding.train(devdata, phidh, phidm, mapdh, mapdm, pptype='for')
+devencode = bilme.BilinearMaxentFeatEncoding.train(devdata, phidh, phidm, mapdh, mapdm, pptype=ppt)
 devtoks = devencode.train_toks()
 devtokens = [(co.word_features(t),l) for t,l in devencode.train_toks()]
 print "total development examples for the pptype - None ", len(devtoks)
@@ -52,47 +57,47 @@ def minlog(L):
     return val
 if inp == 'None':
     print 'calling function type ', inp, ' WindowsErrorth tau = ', tau, ' and eta = ', eta
-    cl = bilme.BilinearMaxent.train(traintoks, encoding, max_iter=100, eta=eta, devset=devtoks, devencode=devencode, tau=tau)
-    np.savetxt('bdevacc'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+'.txt', cl[3], fmt='%f')
-    np.savetxt('bobjective'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+'.txt', cl[2], fmt='%f')
-    np.savetxt('btracc'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+'.txt', cl[1], fmt='%f')
-    np.savetxt('bilwtbn'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+'.txt', cl[0].weight_bn(), fmt='%f')
-    np.savetxt('bilwtbv'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+'.txt', cl[0].weight_bv(), fmt='%f')
+    cl = bilme.BilinearMaxent.train(traintoks, encoding, max_iter=numbers, eta=eta, devset=devtoks, devencode=devencode, tau=tau)
+    np.savetxt('bil-models/bdevacc'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+str(ppt)+'.txt', cl[3], fmt='%f')
+    np.savetxt('bil-models/bobjective'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+str(ppt)+'.txt', cl[2], fmt='%f')
+    np.savetxt('bil-models/btracc'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+str(ppt)+'.txt', cl[1], fmt='%f')
+    np.savetxt('bil-models/bilwtbn'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+str(ppt)+'.txt', cl[0].weight_bn(), fmt='%f')
+    np.savetxt('bil-models/bilwtbv'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+str(ppt)+'.txt', cl[0].weight_bv(), fmt='%f')
 
 elif inp == 'l2':
     print 'calling function type ', inp, ' and tau = ', tau, ' and eta = ', eta
-    cl = bilme.BilinearMaxent.train(traintoks, encoding, max_iter=30, eta=eta, devset=devtoks, devencode=devencode, tau=tau, penalty='l2')
-    np.savetxt('bdevacc'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+'.txt', cl[3], fmt='%f')
-    np.savetxt('bobjective'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+'.txt', cl[2], fmt='%f')
-    np.savetxt('btracc'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+'.txt', cl[1], fmt='%f')
-    np.savetxt('bilwtbn'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+'.txt', cl[0].weight_bn(), fmt='%f')
-    np.savetxt('bilwtbv'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+'.txt', cl[0].weight_bv(), fmt='%f')
+    cl = bilme.BilinearMaxent.train(traintoks, encoding, max_iter=numbers, eta=eta, devset=devtoks, devencode=devencode, tau=tau, penalty='l2')
+    np.savetxt('bil-models/bdevacc'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+str(ppt)+'.txt', cl[3], fmt='%f')
+    np.savetxt('bil-models/bobjective'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+str(ppt)+'.txt', cl[2], fmt='%f')
+    np.savetxt('bil-models/btracc'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+str(ppt)+'.txt', cl[1], fmt='%f')
+    np.savetxt('bil-models/bilwtbn'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+str(ppt)+'.txt', cl[0].weight_bn(), fmt='%f')
+    np.savetxt('bil-models/bilwtbv'+inp+str(samples)+'tau'+str(tau)+'eta'+str(eta)+str(ppt)+'.txt', cl[0].weight_bv(), fmt='%f')
 
 elif inp == 'l1':
-    print 'calling function type ', inp, ' and tau = ', tau, ' and eta = ', eta
-    cl = bilme.BilinearMaxent.train(traintoks, encoding, max_iter=30, LC=eta, devset=devtoks, devencode=devencode, tau=tau, penalty='l1')
-    np.savetxt('bdevacc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[3], fmt='%f')
-    np.savetxt('bobjective'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[2], fmt='%f')
-    np.savetxt('btracc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[1], fmt='%f')
-    np.savetxt('bilwtbn'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[0].weight_bn(), fmt='%f')
-    np.savetxt('bilwtbv'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[0].weight_bv(), fmt='%f')
+    print 'calling function type ', inp, ' and tau = ', tau, ' and LC = ', eta
+    cl = bilme.BilinearMaxent.train(traintoks, encoding, max_iter=numbers, LC=eta, devset=devtoks, devencode=devencode, tau=tau, penalty='l1')
+    np.savetxt('bil-models/bdevacc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[3], fmt='%f')
+    np.savetxt('bil-models/bobjective'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[2], fmt='%f')
+    np.savetxt('bil-models/btracc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[1], fmt='%f')
+    np.savetxt('bil-models/bilwtbn'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[0].weight_bn(), fmt='%f')
+    np.savetxt('bil-models/bilwtbv'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[0].weight_bv(), fmt='%f')
 
 elif inp == 'l2p':
-    print 'calling function type ', inp, ' and tau = ', tau, ' and eta = ', eta
-    cl = bilme.BilinearMaxent.train(traintoks, encoding, max_iter=30, LC=eta, devset=devtoks, devencode=devencode, tau=tau, penalty='l2p')
-    np.savetxt('bdevacc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[3], fmt='%f')
-    np.savetxt('bobjective'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[2], fmt='%f')
-    np.savetxt('btracc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[1], fmt='%f')
-    np.savetxt('bilwtbn'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[0].weight_bn(), fmt='%f')
-    np.savetxt('bilwtbv'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[0].weight_bv(), fmt='%f')
+    print 'calling function type ', inp, ' and tau = ', tau, ' and LC = ', eta
+    cl = bilme.BilinearMaxent.train(traintoks, encoding, max_iter=numbers, LC=eta, devset=devtoks, devencode=devencode, tau=tau, penalty='l2p')
+    np.savetxt('bil-models/bdevacc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[3], fmt='%f')
+    np.savetxt('bil-models/bobjective'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[2], fmt='%f')
+    np.savetxt('bil-models/btracc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[1], fmt='%f')
+    np.savetxt('bil-models/bilwtbn'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[0].weight_bn(), fmt='%f')
+    np.savetxt('bil-models/bilwtbv'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[0].weight_bv(), fmt='%f')
 
 
 elif inp == 'nn':
-    print 'calling function type ', inp, ' and tau = ', tau, ' and eta = ', eta
-    cl = bilme.BilinearMaxent.train(traintoks, encoding, max_iter=500, LC=eta, devset=devtoks, devencode=devencode, tau=tau, penalty='nn')
-    np.savetxt('bdevacc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[3], fmt='%f')
-    np.savetxt('bobjective'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[2], fmt='%f')
-    np.savetxt('btracc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[1], fmt='%f')
-    np.savetxt('bilwtbn'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[0].weight_bn(), fmt='%f')
-    np.savetxt('bilwtbv'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+'.txt', cl[0].weight_bv(), fmt='%f')
+    print 'calling function type ', inp, ' and tau = ', tau, ' and LC = ', eta
+    cl = bilme.BilinearMaxent.train(traintoks, encoding, max_iter=numbers, LC=eta, devset=devtoks, devencode=devencode, tau=tau, penalty='nn')
+    np.savetxt('bil-models/bdevacc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[3], fmt='%f')
+    np.savetxt('bil-models/bobjective'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[2], fmt='%f')
+    np.savetxt('bil-models/btracc'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[1], fmt='%f')
+    np.savetxt('bil-models/bilwtbn'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[0].weight_bn(), fmt='%f')
+    np.savetxt('bil-models/bilwtbv'+inp+str(samples)+'tau'+str(tau)+'lc'+str(eta)+str(ppt)+'.txt', cl[0].weight_bv(), fmt='%f')
 
