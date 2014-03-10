1868ed9edc733a24d2f55d9517bfee86586e8116
f00barin <pranava.madhyastha@gmail.com>
2014-03-10 13:20:30 +0100

    convergence test added

--- graphitlin.py
+++ graphitlin.py
@@ -13,6 +13,7 @@ bestlc = {}
 taudevacc = dd(list)
 taulcdict = dd(list)
 taunormdict = dd(list)
+bestscoresdict = dd(list)
 direc = sys.argv[1]
 inp = sys.argv[2]
 maxobj = float(sys.argv[3])
@@ -21,7 +22,7 @@ os.chdir(direc)
 for files in glob.glob("devaccl2proximal20801*with.txt"):
 
     try:
-        base = re.findall("devacc|l1|l2proximal|\d{3,5}|tau[e0-9\-\.]+|lc[e0-9\.-]+|LC[e0-9\.-]+|with", files)
+        base = re.findall("devacc|l1|l2f|l2proximal|\d{3,5}|tau[e0-9\-\.]+|lc[e0-9\.-]+|LC[e0-9\.-]+|with", files)
 
         sample = int(base[2])
         ppt = str(base[5])
@@ -37,9 +38,20 @@ for files in glob.glob("devaccl2proximal20801*with.txt"):
         tracc = np.loadtxt('tracc'+base[1]+base[2]+base[3]+base[4]+base[5]+'.txt')
 
     #    iteration = scores.argmax() + 1
-        indicator = np.sort(objective)[-1]
-        if indicator < maxobj:
+#        indicator = np.sort(objective)[-1]
+        if len(objective) >= 99:
             objcordlist = []
+
+            convlist = convergence(objective)
+
+            bestdevacc = ()
+            for ind in convlist:
+                if bestdevacc[1] > convlist[ind]:
+                    bestdevacc = (ind, convlist[ind])
+
+            bestscoresdict[float(tau)].append((float(lc), bestdevacc))
+
+
 
             for ind, val in enumerate(objective):
                 objcordlist.append((ind+1, val))
@@ -47,7 +59,6 @@ for files in glob.glob("devaccl2proximal20801*with.txt"):
             taulcdict[float(tau)].append((float(lc), objcordlist))
             taudevacc[float(tau)].append((float(lc), scores))
             taunormdict[float(tau)].append((float(lc), norm))
-
     except:
         continue
 
@@ -90,11 +101,29 @@ def printtop(val):
     print ('    height=\\textwidth,')
     print ('    width=\\textwidth,')
     print ('    xlabel={Iterations},')
-    print ('    ylabel={Objective},')
+    print ('    ylabel={objective},')
     print ('    legend style={at={(0.5, -0.5)}, anchor=west},')
     print ('    ymajorgrids=true,')
     print ('    xmajorgrids=true,')
     print ('    grid style=dashed,]')
+
+
+def convergence(objset):
+    conv_list = []
+    for epoch in xrange(len(objset)):
+        if epoch > 0:
+            if (objset[epoch]-objset[epoch-1]) < 0.005:
+                if len(conv_list) > 0:
+                    if (epoch - conv_list[len(conv_list)-1]) < 2:
+                        conv_list.append(epoch)
+                    else:
+                        conv_list = []
+                else:
+                    conv_list.append(epoch)
+    if len(conv_list) > 2:
+        return conv_list
+    else:
+        return []
 
 def printbottom(bestset):
     print ('\\end{axis}')
@@ -119,10 +148,13 @@ def printdevacc(bestlc):
     bestnormlist = []
     for tau in np.sort(bestlc.keys()):
         lc = bestlc[tau][0]
-        scoredict = dict(taudevacc[tau])
+#        scoredict = dict(taudevacc[tau])
         normdict = dict(taunormdict[tau])
-        best = (scoredict[lc]).max()
-        itr = (scoredict[lc]).argmax()
+        scoredict = dict(bestscoresdict[tau])
+        best = (scoredict[lc])[1]
+        itr = (scoredict[lc])[0]
+#        best = (scoredict[lc]).max()
+#        itr = (scoredict[lc]).argmax()
         optnorm = (normdict[lc])[itr]
         bestscorelist.append((tau, best))
         bestiterlist.append(((tau,best), itr))
